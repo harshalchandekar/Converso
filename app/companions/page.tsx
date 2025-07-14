c738a4162febc4a3fa6352b034f22bc1eaad7815
Harshal Chandekar <harshalchandekar2204@gmail.com>
2025-07-15 00:12:31 +0530

    implemented navbar

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,4 +1,4 @@
-const Companion = () => {
+const Companions = () => {
     return(
         <div>
             <h1>companion</h1>
@@ -6,4 +6,4 @@ const Companion = () => {
     )
 }
 
-export default Companion;
+export default Companions;
