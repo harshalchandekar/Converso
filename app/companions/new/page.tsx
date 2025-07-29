33343cd286c48813dee761638ef6632094b400cb
Harshal Chandekar <harshalchandekar2204@gmail.com>
2025-07-29 19:08:40 +0530

    Implemented the companion form

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,9 +1,15 @@
+import CompanionForm from "@/components/CompanionForm"
+
 const NewCompanion = () => {
     return(
-        <div>
-            <h1>NewCompanion</h1>
-        </div>
+        <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+            <article className="w-full gap-4 flex flex-col ">
+                <h1>Companion Builder</h1>
+
+                <CompanionForm />
+            </article>
+        </main>
     )
 }
 
-export default NewCompanion
+export default NewCompanion;
