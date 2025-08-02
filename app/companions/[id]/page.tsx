52284e371d9d2aa2ced127dceeb913544fe62957
Harshal Chandekar <harshalchandekar2204@gmail.com>
2025-08-02 11:46:37 +0530

    Implemented vapi agent

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CompanionComponent from "@/components/CompanionComponents";
 import { getCompanion } from "@/lib/actions/companion.actions";
 import { getSubjectColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
@@ -10,11 +11,15 @@ interface CompanionSessionPageProps {
 
 const CompanionSession = async({params} : CompanionSessionPageProps) => {
     const {id} = await params;
-    const {name, subject,title,topic, duration} = await getCompanion(id);
+    const companion = await getCompanion(id);
     const user = await currentUser();
+
+    const {name, subject,title,topic, duration} = companion;
 
     if(!user) redirect('/sign-in');
     if(!name) redirect ('/companions')
+
+        
 
 
     return(
@@ -39,6 +44,11 @@ const CompanionSession = async({params} : CompanionSessionPageProps) => {
                     {duration} minutes
                 </div>
             </article>
+            <CompanionComponent
+            {...companion}
+            companionId={id}
+            userName={user.firstName!}
+            userImage={user.imageUrl!} />
         </main>
     )
 }
